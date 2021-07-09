6c05d0ea70595b9ab599ac38f62e13bf3030dd6b
nacho <nacholeon30@hotmail.com>
2021-07-09 16:43:58 -0300

    basic working calculator, basic css structure

--- scripts/main.js
+++ scripts/main.js
@@ -15,15 +15,77 @@ function divide(a,b) {
 }
 
 function operate(op,a,b) {
+    a = + a;
+    b = + b;
     switch(op) {
-        case '+': return a+b;
+        case '+': return add(a,b);
             break;
-        case '-': return a-b;
+        case '-': return subtract(a,b);
             break;
-        case '*': return a*b;
+        case '*': return multiply(a,b);
             break;
-        case '/': return a/b;
+        case '/': return divide(a,b);
             break;
         default: return 'Syntax error!';
     }
 }
+
+
+let displayValue = '';
+
+const displayScreen = document.querySelector('#display');
+
+let storedOperator = '';
+
+let storedValue = '';
+
+let nextTimeClean = false;
+
+
+//NUMBER BUTTONS
+const digBtns = document.querySelectorAll('.digit');
+digBtns.forEach( btn => btn.addEventListener( 'click' , e => {
+    if(nextTimeClean){
+        displayScreen.textContent = '';
+        displayValue = '';
+        nextTimeClean = false;
+    }
+    displayValue+=e.target.innerText;
+    displayScreen.textContent = displayValue;
+} ) );
+
+//OPERATIONS BUTTONS
+const opBtns = document.querySelectorAll('.operator');
+opBtns.forEach( btn => btn.addEventListener( 'click' , e => {
+    if(storedOperator){
+        displayScreen.textContent = storedValue = operate(storedOperator,storedValue,displayValue);
+        displayValue = '';
+    }
+    else{
+        storedValue = displayValue;
+        displayValue = '';
+    }
+    storedOperator = e.target.innerText;
+    nextTimeClean = true;
+} ) );
+
+//CLEAR BUTTON
+const clrBtn = document.querySelector('#clear_btn');
+clrBtn.addEventListener( 'click' , e => {
+    displayScreen.textContent = '';
+    displayValue = '';
+    storedOperator = '';
+    storedValue = '';
+    nextTimeClean = false;
+} )
+
+//EQUAL BUTTON
+const eqlBtn = document.querySelector('#equal_btn');
+eqlBtn.addEventListener( 'click' , e => {
+    if( storedOperator!=='' && storedValue!=='' && displayValue!=='' ){
+        displayScreen.textContent = displayValue = operate(storedOperator,storedValue,displayValue);
+        storedValue = '';
+        nextTimeClean = true;
+    }
+    storedOperator = '';
+} );
