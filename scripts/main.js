0c6d4e209cacbd64251ff5a4a956e2626b9aa4f4
nacho <nacholeon30@hotmail.com>
2021-07-10 11:47:03 -0300

    limit digits function finished, added math error in case of division by 0

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,9 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if(b===0) {
+        return 'Math ERROR'
+    }
     return a/b;
 }
 
@@ -31,7 +34,10 @@ function operate(op,a,b) {
 }
 
 function limitDigits(num) {
-    if(num>=100000000000){
+    if(num==='Math ERROR') {
+        return num;
+    }
+    if(num>=100000000000) {
         let e = 0;
         while(num>=10){
             num = num/10;
@@ -40,7 +46,7 @@ function limitDigits(num) {
         num = Math.round(num*1000)/1000;
         return `${num}e${e}`;
     }
-    if(num<0.000000001){
+    if(num<0.000000001) {
         let e = 0;
         while(num<1){
             num = num*10;
@@ -49,8 +55,12 @@ function limitDigits(num) {
         num = Math.round(num*1000)/1000;
         return `${num}e${e}`;
     }
-    if(String(num).length>11){
-        return num.toPrecision(10);   //no messirve, hacer de otra forma
+    if(String(num).length>11) {
+        let index = String(num).indexOf('.');
+        if(index>10) {
+            return Math.round(num);
+        }
+        return Math.round(num*(10**(10-index)))/(10**(10-index));
     }
     return num;
 }
@@ -70,12 +80,12 @@ let nextTimeClean = false;
 //NUMBER BUTTONS
 const digBtns = document.querySelectorAll('.digit');
 digBtns.forEach( btn => btn.addEventListener( 'click' , e => {
-    if(nextTimeClean){
+    if(nextTimeClean) {
         displayScreen.textContent = '';
         displayValue = '';
         nextTimeClean = false;
     }
-    if(displayValue.length>10){
+    if(displayValue.length>10) {
         return;
     }
     displayValue+=e.target.innerText;
@@ -85,35 +95,46 @@ digBtns.forEach( btn => btn.addEventListener( 'click' , e => {
 //OPERATIONS BUTTONS
 const opBtns = document.querySelectorAll('.operator');
 opBtns.forEach( btn => btn.addEventListener( 'click' , e => {
-    if(storedOperator){
+    if(storedOperator) {
         displayScreen.textContent = storedValue = operate(storedOperator,storedValue,displayValue);
         displayValue = '';
     }
-    else{
+    else {
         storedValue = displayValue;
         displayValue = '';
     }
     storedOperator = e.target.innerText;
     nextTimeClean = true;
+    if(storedValue==='Math ERROR') {
+        clearMemory();
+    }
 } ) );
 
 //CLEAR BUTTON
 const clrBtn = document.querySelector('#clear_btn');
-clrBtn.addEventListener( 'click' , e => {
+clrBtn.addEventListener( 'click' , () => {
+    clearMemory();
     displayScreen.textContent = '';
-    displayValue = '';
-    storedOperator = '';
-    storedValue = '';
-    nextTimeClean = false;
-} )
+} );
 
 //EQUAL BUTTON
 const eqlBtn = document.querySelector('#equal_btn');
 eqlBtn.addEventListener( 'click' , e => {
-    if( storedOperator!=='' && storedValue!=='' && displayValue!=='' ){
+    if( storedOperator!=='' && storedValue!=='' && displayValue!=='' ) {
         displayScreen.textContent = displayValue = operate(storedOperator,storedValue,displayValue);
         storedValue = '';
         nextTimeClean = true;
     }
     storedOperator = '';
+    if(displayValue==='Math ERROR') {
+        clearMemory();
+    }
 } );
+
+
+function clearMemory() {
+    displayValue = '';
+    storedOperator = '';
+    storedValue = '';
+    nextTimeClean = true;
+}
